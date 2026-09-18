dfc4cafb1518d572cad96c8ad5631d0da2e16fd3
Paul J Manoogian <paul@manoogian.net>
2026-09-18 10:11:49 -0400

    Release Android alpha 0.23.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.manoogianmedia.studiorack"
         minSdk = 26
         targetSdk = 35
-        versionCode = 79
-        versionName = "0.23.0"
+        versionCode = 80
+        versionName = "0.23.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
